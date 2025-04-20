66b103e1cd5ac93caece22978a832b986e7e309f
ZygoteCode <zygotecode@proton.me>
2025-04-20 21:48:15 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MySqlDumper
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "MySqlDumper V1 | Made by https://github.com/GabryB03/";
+            Logger.ShowInfo("Welcome to MySqlDumper V1, made by https://github.com/GabryB03/");
+
+            string ip = Logger.RequestInfo("Please, insert the server host / IP address");
+            string database = Logger.RequestInfo("Please, insert the database name to dump");
+            string user = Logger.RequestInfo("Please, insert the username (you can leave this empty if no user is allowed)");
+            string password = Logger.RequestInfo("Please, insert the password (you can leave this empty if no password is allowed)");
+            string port = Logger.RequestInfo("Please, insert the port (you can leave this empty for default 3306 port)");
+
+            if (port == "")
+            {
+                port = "3306";
+            }
+
+            if (!Logger.RequestConfirmation("Are you sure you want to dump the database? The dump will be saved in the \"database.sql\" file."))
+            {
+                Logger.ShowWarning("You refused to dump the database. Please, press ENTER to exit from the program.");
+                Console.ReadLine();
+                return;
+            }
+
+            Logger.ShowWarning("Dumping the database, please wait a while.");
+
+            try
+            {
+                SqlDumper sqlDumper = new SqlDumper(ip, database, user, password, port);
+                sqlDumper.DumpDatabase("database.sql");
+                Logger.ShowSuccess("The database has been succesfully dumped! Press ENTER to exit from the program.");
+            }
+            catch (Exception ex)
+            {
+                string completeReport = "\r\nException Message: " + ex.Message + "\r\nException Source: " + ex.Source + "\r\nException StackTrace: " + ex.StackTrace + "\r\nException Method: " + ex.TargetSite.Name + "\r\nException Class: " + ex.TargetSite.DeclaringType.Name;
+                Logger.ShowError($"An error occured while trying to dump the database. Please, report those info in GitHub opening an issue:\r\n\r\n{completeReport}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SqlDumper.cs
@@ -0,0 +1,148 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text;
+
+namespace MySqlDumper
+{
+    public class SqlDumper
+    {
+        private string _ip, _user, _password, _database, _port;
+        private int _timeout = 99999999;
+
+        public SqlDumper(string ip, string database, string user = "", string password = "", string port = "3306")
+        {
+            _ip = ip;
+            _database = database;
+            _user = user;
+            _password = password;
+            _port = port;
+        }
+
+        public void DumpDatabase(string filePath)
+        {
+            using (MySqlConnection conn = new MySqlConnection(
+                $"server={_ip};user={_user};password={_password};database={_database};port={_port};" +
+                $"Connection Timeout={_timeout};default command timeout={_timeout}"
+            ))
+            {
+                conn.Open();
+
+                DataTable tables = conn.GetSchema("Tables");
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"CREATE DATABASE IF NOT EXISTS `{_database}`;");
+                sb.AppendLine($"USE `{_database}`;");
+
+                Parallel.ForEach(tables.AsEnumerable(), row =>
+                {
+                    string tableName = row["TABLE_NAME"].ToString();
+
+                    using (MySqlConnection innerConn = new MySqlConnection(
+                        $"server={_ip};user={_user};password={_password};database={_database};port={_port};" +
+                        $"Connection Timeout={_timeout};default command timeout={_timeout}"
+                    ))
+                    {
+                        innerConn.Open();
+
+                        using (MySqlCommand cmd = new MySqlCommand($"SHOW CREATE TABLE `{tableName}`;", innerConn))
+                        {
+                            cmd.CommandTimeout = _timeout;
+
+                            using (MySqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+                                    string createTable = reader["Create Table"].ToString();
+
+                                    lock (sb)
+                                    {
+                                        sb.AppendLine(createTable + ";");
+                                    }
+                                }
+                            }
+                        }
+
+                        using (MySqlCommand cmd = new MySqlCommand($"SELECT * FROM `{tableName}`;", innerConn))
+                        {
+                            cmd.CommandTimeout = _timeout;
+
+                            using (MySqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                List<string> batchInserts = new List<string>();
+
+                                while (reader.Read())
+                                {
+                                    StringBuilder insert = new StringBuilder($"INSERT INTO `{tableName}` VALUES(");
+
+                                    for (int i = 0; i < reader.FieldCount; i++)
+                                    {
+                                        if (i > 0)
+                                        {
+                                            insert.Append(",");
+                                        }
+
+                                        if (reader.IsDBNull(i))
+                                        {
+                                            insert.Append("NULL");
+                                        }
+                                        else
+                                        {
+                                            object value = reader.GetValue(i);
+
+                                            if (value is string)
+                                            {
+                                                insert.Append($"'{((string)value).Replace("'", "''")}'");
+                                            }
+                                            else if (value is DateTime)
+                                            {
+                                                insert.Append($"'{((DateTime)value).ToString("yyyy-MM-dd")}'");
+                                            }
+                                            else if (value is decimal || value is float || value is double)
+                                            {
+                                                insert.Append(value.ToString().Replace(",", "."));
+                                            }
+                                            else
+                                            {
+                                                insert.Append(value.ToString());
+                                            }
+                                        }
+                                    }
+
+                                    insert.Append(");");
+                                    batchInserts.Add(insert.ToString());
+
+                                    if (batchInserts.Count >= 1000)
+                                    {
+                                        lock (sb)
+                                        {
+                                            sb.AppendLine(string.Join("\n", batchInserts));
+                                        }
+
+                                        batchInserts.Clear();
+                                    }
+                                }
+
+                                if (batchInserts.Count > 0)
+                                {
+                                    lock (sb)
+                                    {
+                                        sb.AppendLine(string.Join("\n", batchInserts));
+                                    }
+                                }
+                            }
+                        }
+
+                        lock (sb)
+                        {
+                            sb.AppendLine();
+                        }
+                    }
+                });
+
+                using (StreamWriter file = new StreamWriter(filePath))
+                {
+                    file.Write(sb.ToString());
+                }
+            }
+        }
+    }
+}
